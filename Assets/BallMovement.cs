66b6b6781d18fe79fe0af043ed2bbf80066f28be
Jordan E. Kidd <jordanekidd@gmail.com>
2015-03-23 12:40:02 -0400

    initial commit

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+	public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+		// Initial Volocity
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+	float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "RacketLeft") {
+			float y = hitFactor(transform.position,
+			                    col.transform.position,
+			                    col.collider.bounds.size.y);
+
+			Vector2 dir = new Vector2(1, y).normalized;
+
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+
+		if (col.gameObject.name == "RacketRight") {
+			float y = hitFactor(transform.position,
+			                    col.transform.position,
+			                    col.collider.bounds.size.y);
+			
+			Vector2 dir = new Vector2(-1, y).normalized;
+
+			GetComponent<Rigidbody2D>().velocity = dir * speed;
+		}
+	}
+}
